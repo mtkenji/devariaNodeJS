4e9a5d4cac427abc4922773284f4f7c36d765fdc
Marcos Takata <mt.kenji@gmail.com>
2023-11-24 17:38:32 -0300

    atualizacao em operadores

--- operadores-e-functions/app.js
+++ operadores-e-functions/app.js
@@ -4,42 +4,62 @@ const readLine = require('readline').createInterface({
 });
 
 const validarNumeroInformado = (numero)=> {
-    try 
-    {
-        return Number.parseFloat(numero);
-    } catch(e){
+    
+    const resultado = Number.parseFloat(numero);
+    console.log('resultado: '+resultado);
+    if (resultado == NaN)
         console.log('O numero informado não é válido.');    
-    }    
+    else
+        return resultado;
+}
 
+const validarOperador = (operador) => {
+    switch (operador) {
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+        case "%":
+            return operador;
+            break;
+
+        default:
+            console.log('Operador inválido.');
+            return null;
+            break;
+    }
 }
 
 readLine.question('Favor informar um número: ', (numero1) => {
     const numeroValidado1 = validarNumeroInformado(numero1);
     
-    if (numeroValidado1 ){
+    if (numeroValidado1){
         readLine.question('Favor informar outro número: ',  (numero2) => {
-        const numeroValidado2 = validarNumeroInformado(numero2);
+            const numeroValidado2 = validarNumeroInformado(numero2);
 
-
-        if (numeroValidado2)
-        {
-            readLine.question('Favor informar o operador: ', (operador) => {
-                switch (operador){
-                    case "+":
-                    case "-":
-                    case "*":
-                    case "/":
-                        break;
-
-                    default:
-                        "Operador inválido.";
-                        break;
-                }
-
-            });
-
-        }
+            if (numeroValidado2)
+            {
+                readLine.question('Favor informar o operador: ', (operador) => {
+                    const operadorValidado = validarOperador(operador);
+                    
+                    if (operadorValidado){
+                        switch (operadorValidado){
+                            case "+": console.log(`O operador selecionado foi de Adição: ${numeroValidado1} + ${numeroValidado2} =  ${numeroValidado1 + numeroValidado2}.`);
+                                break;
+                            case "-": console.log(`O operador selecionado foi de Subtração: ${numeroValidado1} - ${numeroValidado2} =  ${numeroValidado1 - numeroValidado2}.`);
+                                break;
+                            case "*": console.log(`O operador selecionado foi de Multiplicação: ${numeroValidado1} * ${numeroValidado2} =  ${numeroValidado1 * numeroValidado2}.`);
+                                break;
+                            case "/": console.log(`O operador selecionado foi de Divisão: ${numeroValidado1} / ${numeroValidado2} =  ${numeroValidado1 / numeroValidado2}.`);
+                                break;
+                            case "%": console.log(`O operador selecionado foi de Módulo: ${numeroValidado1} % ${numeroValidado2} =  ${numeroValidado1 % numeroValidado2}.`);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                });
+            }
         });
     }
-
 });
